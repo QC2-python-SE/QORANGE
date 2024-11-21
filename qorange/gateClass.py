e16d57f2dd4bcf4b40ae45a0cc4bc29c4e0b00d1
srinjoyganguly <srinjoyganguly@gmail.com>
2024-11-21 14:03:42 +0000

    implemented two qubit quantum gates

--- qorange/gateClass.py
+++ qorange/gateClass.py
@@ -49,7 +49,36 @@ def T(singleQubitGate):
 
     def __init__(self):
         singleQubitGate.__init__(self, np.array([[1, 0], [0, 1/np.sqrt(2)+1j/np.sqrt(2)]]))
+
+def CNOT(twoQubitGate):
+
+    def __init__(self):
+        twoQubitGate.__init__(self, np.array([
+            [1, 0, 0, 0],  # |00⟩ -> |00⟩
+            [0, 1, 0, 0],  # |01⟩ -> |01⟩
+            [0, 0, 0, 1],  # |10⟩ -> |11⟩
+            [0, 0, 1, 0]   # |11⟩ -> |10⟩
+        ]))
+
+def CZ(twoQubitGate):
+
+    def __init__(self):
+        twoQubitGate.__init__(self, np.array([
+            [1, 0, 0, 0],  
+            [0, 1, 0, 0],  
+            [0, 0, 1, 0],  
+            [0, 0, 0, -1]   
+        ]))
     
+def SWAP(twoQubitGate):
+
+    def __init__(self):
+        twoQubitGate.__init__(self, np.array([
+            [1, 0, 0, 0],  # |00⟩ -> |00⟩
+            [0, 0, 1, 0],  # |01⟩ -> |01⟩
+            [0, 1, 0, 0],  # |10⟩ -> |11⟩
+            [0, 0, 0, 1]   # |11⟩ -> |10⟩
+        ]))
 
 
 
