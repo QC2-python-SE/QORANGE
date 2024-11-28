9e7ebfa94d25c2d61bb75ad0293e0cb5c10110ec
Cathy <catherine_d2005@hotmail.co.uk>
2024-11-28 17:17:34 +0000

    Skeleton functions for gates unit tests

---
+++ tests/test_gates.py
@@ -0,0 +1,71 @@
+###########################
+# 1-QUBIT GATES
+###########################
+
+def test_apply_gate_identity():
+    '''
+    TODO
+    '''
+    pass
+
+def test_apply_gate_pauli_x():
+    '''
+    TODO
+    '''
+    pass
+
+def test_apply_gate_pauli_y():
+    '''
+    TODO
+    '''
+    pass
+
+def test_apply_gate_pauli_z():
+    '''
+    TODO
+    '''
+    pass
+
+def test_apply_gate_hadamard():
+    '''
+    TODO
+    '''
+    pass
+
+def test_apply_gate_s():
+    '''
+    TODO
+    '''
+    pass
+
+def test_apply_gate_t():
+    '''
+    TODO
+    '''
+    pass
+
+###########################
+# CONTROLLED GATES
+###########################
+
+def test_apply_gate_cnot():
+    '''
+    TODO
+    '''
+    pass
+
+def test_apply_gate_cz():
+    '''
+    TODO
+    '''
+    pass
+
+###########################
+# 2-QUBIT GATES
+###########################
+
+def test_apply_gate_swap():
+    '''
+    TODO
+    '''
+    pass
