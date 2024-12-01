6829756202a29e0e71606bdcee141fc2981c72d2
srinjoyganguly <srinjoyganguly@gmail.com>
2024-12-01 17:11:18 +0000

    added test measurement circuit with X on first qubit and SWAP gate

--- tests/test_measurements.py
+++ tests/test_measurements.py
@@ -5,7 +5,7 @@ from qorange.gates import *
 
 def test_circuit_meas_1():
     '''
-    Test the circuit with a Pauli-X gate on the first qubit and a SWAP gate.
+    Test the circuit with a Hadamrd gate on the first qubit and a CNOT gate.
     '''
     # Apply the gates
     circuit = QuantumCircuit()
@@ -14,6 +14,24 @@ def test_circuit_meas_1():
 
     expected_outcome_1 = [0.5, 0.5]
     expected_outcome_2 = [0.5, 0.5]
+    # Measure qubit 1
+    outcome_1 = circuit.measure_qubit_computational(1)
+    # Measure qubit 2
+    outcome_2 = circuit.measure_qubit_computational(2)
+    npt.assert_allclose(outcome_1, expected_outcome_1, rtol=1e-5, atol=1e-8)
+    npt.assert_allclose(outcome_2, expected_outcome_2, rtol=1e-5, atol=1e-8)
+
+def test_circuit_meas_2():
+    '''
+    Test the circuit with a Pauli-X gate on the first qubit and a SWAP gate.
+    '''
+    # Apply the gates
+    circuit = QuantumCircuit()
+    circuit.apply_gate(PauliX(), 1)  # Apply X gate to the first qubit
+    circuit.apply_gate(SWAP())  # Apply SWAP gate between qubits 1 and 2
+
+    expected_outcome_1 = [1, 0]
+    expected_outcome_2 = [0, 1]
     # Measure qubit 1
     outcome_1 = circuit.measure_qubit_computational(1)
     # Measure qubit 2
